562baa9667047e29b6ac2db2058b0c4db6a59eb8
Павел <@MAINPC>
2020-10-31 22:02:09 +0300

    Добавьте файлы проекта.

---
+++ stm32_ads1220/stm32_ads1220.ino
@@ -0,0 +1,15 @@
+﻿/*
+ Name:		stm32_ads1220.ino
+ Created:	31.10.2020 22:02:04
+ Author:	Павел
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
